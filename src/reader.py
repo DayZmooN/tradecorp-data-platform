3160c60b55f052fe5627450dbb7a89116d855e41
DayZmooN <karim-du01@live.fr>
2026-09-03 10:39:13 +0200

    update : reader.py download_files_to_local()

--- src/reader.py
+++ src/reader.py
@@ -3,30 +3,48 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client = connection_azure()
 
+# Liste des fichiers CSV
+FILES = [
+    "customers.csv",
+    "orders.csv",
+    "order_details.csv",
+    "products.csv",
+    "categories.csv",
+    "employees.csv",
+    "shippers.csv",
+    "suppliers.csv" 
+]
 
-def download_file_to_local(client,container_name, local_path):
+def download_file_to_local(container_name, local_dir,file_names):
     """Télécharger les fichiers depuis ADLS vers un repertoire local"""
+    client = connection_azure()
+    if client is None:
+            raise Exception("Impossible de se connecter à ADLS")
+    
     container_client = client.get_container_client(container_name)
 
-    
+    # On crée le dossier local s'il n'existe pas
+    os.makedirs(local_dir, exist_ok=True)
+
+
     for blob in container_client.list_blobs():
-        print(blob.name)
-        blob_client = container_client.get_blob_client(blob.name)
-        print(blob_client)
-        download = blob_client.download_blob()
 
-        file_path  = os.path.join(local_path, blob.name)
-        with open(file_path, "wb") as file:
-            
-            file.write(download.readall())
+        if blob.name in file_names:
+            print(f"Téléchargement de {blob.name}")
 
-        print(f"{blob.name} téléchargé")
+            blob_client = container_client.get_blob_client(blob.name)
+            download_stream = blob_client.download_blob()
 
+            local_path = os.path.join(local_dir,blob.name)
 
-download_file_to_local(client, os.environ["AZURE_CONTAINER_RAW"], "/home/jovyan/data")
+            with open(local_path, "wb") as file:
+                file.write(download_stream.readall())
 
+            print(f"{blob.name} téléchargé dans {local_path}")
 
 
-# lire les fichiers country_currency.csv 
+download_file_to_local(os.environ["AZURE_CONTAINER_RAW"], "/home/jovyan/data/raw",FILES)
+
+
+
