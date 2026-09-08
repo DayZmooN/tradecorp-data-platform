3ab23ed911ab4d57892ffb12dc5eaf23fd370153
DayZmooN <karim-du01@live.fr>
2026-09-08 17:03:12 +0200

    Implement data reading and processing in writer.py for enriched orders

--- src/writer.py
+++ src/writer.py
@@ -4,9 +4,30 @@ import os
 
 load_dotenv()
 
-# client =  connection_azure()
+client =  connection_azure()
 STORAGE_ACCOUNT = os.environ['AZURE_STORAGE_ACCOUNT']
 
+# for airflow
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("writer").getOrCreate()
+LOCAL_DIR ="/home/jovyan/data/tmp/tranformer_output"
+LOCAL_OUTPUT = "/home/jovyan/data/tmp/clean_output"
+CONTAINER_CLEAN = os.environ["AZURE_CONTAINER_CLEAN"]
+
+
+FILES = [
+    "enriched"
+]
+
+def files_output(spark ,file, input_dir):
+    dfs = {}
+    for f in file:
+        output_path = os.path.join(input_dir,f)
+        dfs[f] = spark.read.parquet(output_path)
+    return dfs
+
+dfs = files_output(spark, FILES,LOCAL_DIR)
 # Création du fichier Parquet en local puis upload vers Azure
 def upload_files_adls(client, df, local_dir, container_name, azure_path, file_name):
     # Crée le parquet en local
@@ -32,6 +53,9 @@ def upload_files_adls(client, df, local_dir, container_name, azure_path, file_na
 
             print(f"Fichier créé en local : {local_file_path}")
             print(f"Fichier envoyé sur Azure : {azure_path}/{file_name}")
+
+
+upload_files_adls(client,dfs["enriched"],LOCAL_OUTPUT,CONTAINER_CLEAN, "enriched/orders", "order_enriched.parquet")
 
 
 
